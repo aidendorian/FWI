d3faddbc94312440f2b12ba9bff6feae254ef3bc
aidendorian <resorttoaiden@gmail.com>
2025-07-15 15:07:07 +0000

    PDE Residual

--- GAN/Generator/losses.py
+++ GAN/Generator/losses.py
@@ -28,8 +28,46 @@ def pde_res(u, Vp, Vs, density, dx=1., dt=1.):
         density : Density of substructure
         dx and dt: Derivatives
     """
+    B, _, T, W = u.shape
+    H = 5
+    ux = u[:, :5, :, :]
+    uz = u[:, 5:, :, :]
     
-def generator_loss(Discriminator, fake_samples, waveform_pred, waveform_obs, density, lambda_data=1., lambda_pde=1.):
+    u = torch.stack([ux, uz], dim=1)
+    u = u.permute(0, 1, 3, 2, 4)
+    
+    utt_x = (u[:, 0, 2:] - 2*u[:, 0, 1:-1] + u[:, 0, :-2]) / dt**2
+    utt_z = (u[:, 1, 2:] - 2*u[:, 1, 1:-1] + u[:, 1, :-2]) / dt**2
+    utt = torch.stack([utt_x, utt_z], dim=1)
+    
+    density_exp = density.unsqueeze(2).expand_as(utt[:, 0])
+    mu = density*Vs**2
+    lam = density*(Vs**2-2*Vs**2)
+    mu = mu.unsqueeze(2).expand_as(utt[:, 0])
+    lam = lam.unsqueeze(2).expand_as(utt[:, 0])
+    
+    def spatial_deriv(f, dim):
+        return (f[..., 2:] - f[..., :-2]) / (2*dx)
+    
+    u_x = u[:, 0, 1:-1]
+    u_z = u[:, 1, 1:-1]
+    
+    dux_dx = spatial_deriv(u_x, dim=-1)
+    duz_dz = spatial_deriv(u_z, dim=-2)
+    dux_dz = spatial_deriv(u_x, dim=-2)
+    duz_dx = spatial_deriv(u_z, dim=-1)
+    
+    fx = (lam+2*mu)*dux_dx+lam*duz_dz+mu*(dux_dz+duz_dx)
+    fz = (lam+2*mu)*duz_dz+lam*dux_dx+mu*(dux_dz+duz_dx)
+    f = torch.stack([fx, fz], dim=1)
+    
+    utt_cropped = utt[..., 1:-1, 1:-1]
+    density_cropped = density_exp[..., 1:-1, 1:-1]
+    
+    residual = density_cropped.unsqueeze(1)*utt_cropped-f
+    return torch.mean(residual**2)
+    
+def generator_loss(Discriminator, fake_samples, waveform_pred, waveform_obs, Vp, Vs, density, lambda_data=1., lambda_pde=1.):
     """Computes total Generator Loss
 
     Args:
@@ -37,12 +75,14 @@ def generator_loss(Discriminator, fake_samples, waveform_pred, waveform_obs, den
         fake_samples : Samples from Generator
         waveform_pred : Waveform Predicted with Elastic Wave Equation Solver
         waveform_obs : Observed Waveform from Earth Data
+        Vp : P waves
+        Vs : S waves
         density : Density of the substructure
         lambda_data : Data Misfit weight. Defaults to 1.0
         lambda_pde : PDE weight. Defaults to 1.0
     """
     L_adv = adversarial_loss(Discriminator, fake_samples)
     L_data = data_misfit(waveform_pred, waveform_obs)
-    L_pde = pde_res(waveform_pred, density)
+    L_pde = pde_res(waveform_pred, Vp, Vs, density)
     
     return L_adv + lambda_data * L_data + lambda_pde * L_pde
